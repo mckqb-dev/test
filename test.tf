4f831018b8ebdd61c0d1aae7e85bebc8e867b989
Aniket Patel <aniket@Anikets-MacBook-Pro.local>
2017-01-25 14:56:57 -0500

    Add tf file

---
+++ test.tf
@@ -0,0 +1,20 @@
+variable "aws_key" {
+}
+
+variable "aws_secret" {
+}
+
+provider "aws" {
+  access_key = "${var.aws_key}"
+  secret_key = "${var.aws_secret}"
+  region     = "us-east-1"
+}
+#resource "aws_key_pair" "deployer" {
+#    key_name = "deployer-key"
+#    public_key = "${file(var.ssh_pubkey_file)}"
+#}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
